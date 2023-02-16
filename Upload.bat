36a7ec080d1af6992844636f3188287b614e766a
caxmirror <627085892@qq.com>
2023-02-17 00:57:05 +0800

    DoubanCrawler 1.0

---
+++ Upload.bat
@@ -0,0 +1,8 @@
+git init
+git remote add origin https://github.com/caxmirror/DoubanCrawler.git
+git config user.name "caxmirror"
+git config user.email "627085892@qq.com"
+git add .
+git commit -m "DoubanCrawler 1.0"
+git push -u origin master
+pause
